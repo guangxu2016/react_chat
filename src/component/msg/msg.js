f385686776844d671c0eeba0d5680724c33db85b
run <guangxu2016@foxmail.com>
2018-01-20 18:12:38 +0800

    完成消息排序功能

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {connect} from "react-redux"
-import {List} from "antd-mobile";
+import {List, Badge} from "antd-mobile";
 
 @connect(
-    state=>state
+    state => state
 )
 
 class Msg extends React.Component {
     getLast(arr) {
-        return arr[arr.length-1]
+        return arr[arr.length - 1]
     }
 
     render() {
@@ -18,35 +18,49 @@ class Msg extends React.Component {
         const userinfo = this.props.chat.users
         // console.log(this.props)
         const msgGroup = {}
-        this.props.chat.chatmsg.forEach(v=>{
+        this.props.chat.chatmsg.forEach(v => {
             // 判断是否相同，不是返回空数组
             msgGroup[v.chatid] = msgGroup[v.chatid] || []
             msgGroup[v.chatid].push(v)
         })
         console.log(msgGroup)
 
-        const chatList = Object.values(msgGroup)
+        const chatList = Object.values(msgGroup).sort((a, b) => {
+            const a_last = this.getLast(a).create_time
+            const b_last = this.getLast(b).create_time
+            return b_last - a_last
+        })
         // 按照聊天用户分组，根据chatid
         return (
-         <div>
-             <List>
-                 {chatList.map(v=>{
-                     // console.log(v)
-                     const lastItem = this.getLast(v)
-                     const targetId = v[0].from==userid?v[0].to:v[0].from
-                     const name = userinfo[targetId]?userinfo[targetId].name:""
-                     const avatar = userinfo[targetId]?userinfo[targetId].avatar:""
+            <div>
+                <List>
+                    {chatList.map(v => {
+                        // console.log(v)
+                        const lastItem = this.getLast(v)
+                        //v是数组
+                        const targetId = v[0].from == userid ? v[0].to : v[0].from
+                        const name = userinfo[targetId] ? userinfo[targetId].name : ""
+                        const avatar = userinfo[targetId] ? userinfo[targetId].avatar : ""
+                        // 未读
+                        const unreadNum = v.filter(v => !v.read && v.to == userid).length
 
-                     return(<Item
+                        return (<Item
+                                arrow="horizontal"
                                 key={lastItem._id}
+                                extra={<Badge text={unreadNum}></Badge>}
                                 thumb={require(`../image/${userinfo[targetId].avatar}.jpg`)}
+                                onClick={()=>{
+                                        this.props.history.push(`/chat/${targetId}`)
+                                    }
+                                }
                             >
-                             {lastItem.content}
-                         <Brief>{userinfo[targetId].name}</Brief>
-                     </Item>)
-                 })}
-             </List>
-         </div>
+                            {lastItem.content}
+                            {/*获取用户名.*/}
+                            <Brief>{userinfo[targetId].name}</Brief>
+                        </Item>)
+                    })}
+                </List>
+            </div>
         )
     }
 }
