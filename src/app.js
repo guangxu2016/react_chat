dcf991f8890dd6c8ed4522fcf06b63e82640fcd8
run <guangxu2016@foxmail.com>
2018-01-23 13:05:51 +0800

    优化代码

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Login from "./container/login/login";
+import Register from "./container/register/register";
+import BossInfo from "./container/bossinfo/bossinfo";
+import GeniusInfo from "./container/geniusinfo/geniusinfo";
+import Dashboard from "./component/dashboard/dashboard";
+import Chat from "./component/chat/chat";
+import AuthRoute from "./component/authroute/authroute.js"
+import {
+    Route,
+    Switch
+} from "react-router-dom";
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <AuthRoute></AuthRoute>
+                {/*switch只要命中就不渲染*/}
+                <Switch>
+                    <Route path="/bossinfo" component={BossInfo}></Route>
+                    <Route path="/geniusinfo" component={GeniusInfo}></Route>
+                    <Route path="/login" component={Login}></Route>
+                    <Route path="/register" component={Register}></Route>
+                    <Route path="/chat/:user" component={Chat}></Route>
+                    <Route component={Dashboard}></Route>
+                </Switch>
+
+
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -6,21 +6,20 @@ import thunk from "redux-thunk";
 import {Provider} from "react-redux";
 import {
     BrowserRouter,
-    Route,
-    Switch
 } from "react-router-dom";
 
 // 引入文件
 
 import reducer from "./reducer";
 import "./config"
-import Login from "./container/login/login.js";
-import Register from "./container/register/register.js";
-import BossInfo from "./container/bossinfo/bossinfo.js";
-import GeniusInfo from "./container/geniusinfo/geniusinfo.js";
-import AuthRoute from "./component/authroute/authroute.js";
-import Dashboard from "./component/dashboard/dashboard.js";
-import Chat from "./component/chat/chat.js";
+import App from "./app.js";
+// import Login from "./container/login/login.js";
+// import Register from "./container/register/register.js";
+// import BossInfo from "./container/bossinfo/bossinfo.js";
+// import GeniusInfo from "./container/geniusinfo/geniusinfo.js";
+// import AuthRoute from "./component/authroute/authroute.js";
+// import Dashboard from "./component/dashboard/dashboard.js";
+// import Chat from "./component/chat/chat.js";
 
 // 引入css样式
 import "./index.css";
@@ -35,20 +34,7 @@ ReactDom.render(
     // 最外层
     (<Provider store={store}>
         <BrowserRouter>
-            <div>
-                <AuthRoute></AuthRoute>
-                {/*switch只要命中就不渲染*/}
-                <Switch>
-                    <Route path="/bossinfo" component={BossInfo}></Route>
-                    <Route path="/geniusinfo" component={GeniusInfo}></Route>
-                    <Route path="/login" component={Login}></Route>
-                    <Route path="/register" component={Register}></Route>
-                    <Route path="/chat/:user" component={Chat}></Route>
-                    <Route component={Dashboard}></Route>
-                </Switch>
-
-
-            </div>
+            <App></App>
         </BrowserRouter>
 
     </Provider>),
